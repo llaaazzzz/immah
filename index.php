d1490de812d01bf3659653b0ea5dbbdbb60c7e2d
llaaazzzz <71396263+llaaazzzz@users.noreply.github.com>
2020-10-18 21:30:15 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,67 @@
+<!DOCTYPE html>
+<head>
+	<title>
+	Accueil - Immah Vente
+	</title>
+	<meta charset="utf-8">
 
+</head>
+<body>
+	<!-- Haut de page -->
+	<style>
+		<?php 
+			include 'config/css/bases.css'; 
+			include 'config/css/texts.css'; 
+		?>
+	</style>
+
+	<header>
+        <!--Paragraphe de bienvenue-->
+    <p>
+        Bienvenue sur ImmahVente.com, site de e-commerce
+    </p>
+  </header>
+  <!--Menu de navigation (Haut)-->
+  <nav class="menu-nav">
+    <ul>
+      <li class="btn">
+        <a href="//localhost" title="Page d'Accueil">
+          Accueil
+        </a>
+      </li>
+      <li class="btn">
+        <a href="pages/shoes.php" title="Chaussures, Chaussettes">
+          Chaussures
+        </a>
+      </li>
+      <li class="btn">
+        <a href="pages/cloths.php" title="Robes, Pantalons, T-Shirts, ...">
+          Habits
+        </a>
+      </li>
+      <li class="btn">
+        <a href="pages/access.php" title="Bijoux, Montres, Sacs...">
+          Accessoires
+        </a>
+        </li>
+      <li class="btn">
+        <a href="pages/tech.php" title="TV, Téléphones, Tablettes, Montres Connectées...">
+          High-Tech
+        </a>
+          </li>
+      <li class="btn">
+          <a href="pages/contact.php" title="La page pour tout savoir sur nos services">
+          A propos
+        </a>
+      </li>
+    </ul>
+  </nav>
+
+	<section class= "Articles">
+			 
+	</section>
+
+	<?php include 'config/includes/menunavigationp.php' ?>
+
+</body>
+</html>
