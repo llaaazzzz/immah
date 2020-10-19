1c38de1f4a97f9db42416d0e96854555990371a2
llaaazzzz <71396263+llaaazzzz@users.noreply.github.com>
2020-10-19 15:31:07 +0000

    main

--- pages/access.php
+++ pages/access.php
@@ -11,8 +11,6 @@
 
 <!--Site Web-->
 <body>
-    <!--Haut de Page-->
-    <?php include 'Includes/header.php'; ?>
 
     <!--Paragraphe de bienvenue-->
     <header>
@@ -24,7 +22,7 @@
     <nav class="menu-nav">
         <ul>
             <li class="btn">
-                <a href="//localhost" title="Page d'Accueil">
+                <a href="#" title="Page d'Accueil">
                     Accueil
                 </a>
             </li>
@@ -59,5 +57,44 @@
 
 
     <!--Pied de page-->
-	<?php include 'Includes/menunavigationp.php'?>
+	<footer class="menu-nav">
+        <!--Menu de navigation(Bas)-->
+		<ul>
+			<li class="btn">
+				<a href="#" title="Page d'Accueil">
+					Accueil
+				</a>
+			</li>
+			<li class="btn">
+				<a href="/pages/shoes.php" title="Chaussures - Chaussettes">
+					Chaussures
+				</a>
+			</li>
+			<li class="btn">
+				<a href="/pages/cloths.php" title="Robes, Pantalons, T-Shirts, ...">
+					Habits
+				</a>
+			</li>
+			<li class="btn">
+				<a href="/pages/access.php" title="Bijoux, Montres, Sacs...">
+					Accessoires
+				</a>
+			</li>
+			<li class="btn">
+				<a href="/pages/tech.php" title="TV, Téléphones, Tablettes, Montres Connectées...">
+					High-Tech
+				</a>
+			</li>
+			<li class="btn">
+				<a href="/pages/contact.php" title="La page pour tout savoir sur nos services">
+					A propos
+				</a>
+			</li>
+        </ul>
+        
+        <!--Copyright-->
+		<p class="copyright">
+			Immah Vente Togo Copyright &copy; Développé par <strong><a href="mailto:lazking.pro@gmail.com" title="Envoyer un e-mail" id="lienmail">Lazare</a></strong>  - 2020 - Tous Droits Réservés
+		</p>
+	</footer>
 </body>
--- pages/cloths.php
+++ pages/cloths.php
@@ -10,7 +10,14 @@
 <!--Site Web-->
 <body>
     <style>
-        <?php include '../Config/Includes/styles.php' ?>
+        <?php 
+            include '../Config/css/bases.css';
+            include '../Config/css/formulaire.css';
+            include '../Config/css/texts.css';
+            include '../Config/css/style01.css';
+            include '../Config/css/style02.css';
+            include '../Config/css/style03.css';
+        ?>
     </style>
     <!--Menu de navigation (Haut)-->
     <?php include '../config/Includes/menunavigationh.php';?>
